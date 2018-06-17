69ac80c41fdec08366eefa35234da5766ec5800b
Wesley Chang <weschang15@gmail.com>
2018-06-16 23:06:09 -0700

    Configured group exporting index file for mvc architecture

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+/**
+ * * Group exporting
+ *
+ * Use this file to require in all of your modules and then export each one all at once.
+ * This allows you to refactor your require statements across your application. See example:
+ *
+ */
+
+module.exports = {};
---
+++ middlewares/index.js
@@ -0,0 +1,11 @@
+/**
+ * * Group exporting
+ *
+ * Use this file to require in all of your modules and then export each one all at once.
+ * This allows you to refactor your require statements across your application. See example:
+ *
+ */
+
+const { apiErrorHandler } = require("./errorHandlers");
+
+module.exports = { apiErrorHandler };
